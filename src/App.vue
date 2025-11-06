49540a00eeef5d54d8f234bb1e3b55812affe718
space2pacman <space2pacman@gmail.com>
2025-11-06 20:50:05 +0300

    update web

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <b-navbar type="light" variant="light">
+    <b-navbar type="light" variant="light" fixed="top">
       <b-container>
         <b-navbar-brand href="#">Lineage2JS</b-navbar-brand>
 
@@ -23,6 +23,39 @@
     <b-container class="p-0 mt-2">
       <router-view />
     </b-container>
+
+    <footer class="footer mt-auto" style="background-color: black;">
+      <div class="container py-5">
+        <div class="row">
+          <div class="col-lg-6 mb-4">
+            <h5 class="fw-bold mb-3 text-light">
+              <!-- <img src="images/logo.png" width="24" height="24" class="me-2 align-bottom" alt=""> -->
+              Lineage2JS
+            </h5>
+            <p class="text-light">THESE SERVERS ARE GAME EMULATOR WITH ITS IMPROVEMENTS. USAGE OUR SERVICE IS FOR INFORMATION ONLY.</p>
+          </div>
+          <div class="col-lg-3 col-md-6 mb-4">
+            <h6 class="fw-bold text-light">Resources</h6>
+            <ul class="list-unstyled">
+              <li><a class="text-white text-decoration-none" href="/docs/">Documentation</a></li>
+              <li><a class="text-white text-decoration-none" href="/downloads.html">Downloads</a></li>
+              <li><a class="text-white text-decoration-none" href="https://github.com/lineage2js">Source Code</a></li>
+            </ul>
+          </div>
+          <div class="col-lg-3 col-md-6 mb-4">
+            <h6 class="fw-bold text-light">Community</h6>
+            <ul class="list-unstyled">
+              <li><a class="text-white text-decoration-none" href="https://www.youtube.com/@lineage2js">YouTube</a></li>
+              <li><a class="text-white text-decoration-none" href="https://t.me/lineage2js">Telegram</a></li>
+              <li><a class="text-white text-decoration-none" href="https://github.com/lineage2js">GitHub</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="pt-4 mt-4 border-top border-secondary text-center">
+          <p class="text-light mb-0">Copyright © 2025 Lineage2JS. All rights reserved.</p>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="d-flex align-items-center mt-5">
+    <div class="d-flex align-items-center border-bottom vh-100">
       <div>
         <h1>Enter the World of Lineage 2</h1>
         <div class="mt-4">Experience the ultimate MMORPG adventure with enhanced graphics, balanced gameplay and an active community</div>
@@ -22,112 +22,120 @@
       "></div>
     </div>
 
-    <hr class="mt-5">
+    <div class="d-flex align-items-center border-bottom vh-100">
+      <div class="w-100">
+        <h2 class="text-center">Server status</h2>
+        <div class="text-center">Real-time server information and player statistics</div>
+        <b-card-group class="mt-4" deck>
+          <b-card>
+            <div class="d-flex justify-content-between">
+              <div>Server status</div>
+              <div class="text-success font-weight-bold">
+                <b-icon-circle-fill variant="success" />
+                <span class="ml-2">Online</span>
+              </div>
+            </div>
+            <div class="d-flex justify-content-between mt-2">
+              <div>Ping</div>
+              <div>12ms</div>
+            </div>
+            <div class="d-flex justify-content-between mt-2">
+              <div>Version</div>
+              <div>alpha</div>
+            </div>
+          </b-card>
+          <b-card>
+            <div class="d-flex justify-content-between">
+              <div>Online</div>
+              <div class="text-success font-weight-bold">
+                <b-icon-people-fill />
+              </div>
+            </div>
+            <div class="d-flex justify-content-between mt-2">
+              <div>Characters</div>
+              <div>0</div>
+            </div>
+            <div class="d-flex justify-content-between mt-2">
+              <div>Bots</div>
+              <div>0</div>
+            </div>
+          </b-card>
+          <b-card>
+            <div class="d-flex justify-content-between">
+              <div>Server info</div>
+              <div class="text-success font-weight-bold">
+                <b-icon-hdd-stack-fill variant="success" />
+              </div>
+            </div>
+            <div class="d-flex justify-content-between mt-2">
+              <div>Chronicle</div>
+              <div>C1</div>
+            </div>
+            <div class="d-flex justify-content-between mt-2">
+              <div>Rates</div>
+              <div>x1</div>
+            </div>
+          </b-card>
+        </b-card-group>
+      </div>
+    </div>
 
-    <h2 class="text-center mt-5">Server status</h2>
-    <div class="text-center">Real-time server information and player statistics</div>
-    <b-card-group class="mt-4" deck>
-      <b-card>
-        <div class="d-flex justify-content-between">
-          <div>Server status</div>
-          <div class="text-success font-weight-bold">
-            <b-icon-circle-fill variant="success" />
-            <span class="ml-2">Online</span>
-          </div>
-        </div>
-        <div class="d-flex justify-content-between mt-2">
-          <div>Ping</div>
-          <div>12ms</div>
-        </div>
-        <div class="d-flex justify-content-between mt-2">
-          <div>Version</div>
-          <div>0.0.1</div>
-        </div>
-      </b-card>
-      <b-card>
-        <div class="d-flex justify-content-between">
-          <div>Players online</div>
-          <div class="text-success font-weight-bold">
-            <b-icon-people-fill />
-          </div>
-        </div>
-        <h3 class="text-center mt-2">0</h3>
-        <div class="progress">
-          <div class="progress-bar" role="progressbar" style="width: 0%"></div>
-        </div>
-      </b-card>
-      <b-card>
-        <div class="d-flex justify-content-between">
-          <div>Server info</div>
-          <div class="text-success font-weight-bold">
-            <b-icon-hdd-stack-fill variant="success" />
-          </div>
-        </div>
-        <div class="d-flex justify-content-between mt-2">
-          <div>Chronicle</div>
-          <div>C1</div>
-        </div>
-        <div class="d-flex justify-content-between mt-2">
-          <div>Rates</div>
-          <div>x1</div>
-        </div>
-      </b-card>
-    </b-card-group>
-
-    <hr class="mt-5">
-
-    <h2 class="text-center mt-5">Download & Updates</h2>
-    <div class="text-center">Get the latest client and patches to start your adventure</div>
-    <b-card-group class="mt-4" deck>
-      <b-card>
-        <div class="text-center">
-          <b-iconstack font-scale="3">
-            <b-icon stacked icon="circle-fill" variant="success"></b-icon>
-            <b-icon stacked icon="download" variant="light" scale="0.5"></b-icon>
-          </b-iconstack>
-        </div>
-        <h3 class="text-center mt-3">Game client</h3>
-        <div class="text-center">Full game client</div>
-        <div class="mt-3">
-          <div class="d-flex justify-content-between">
-            <div>Size</div>
-            <div class="font-weight-bold">1 GB</div>
-          </div>
-          <div class="d-flex justify-content-between">
-            <div>Version</div>
-            <div class="font-weight-bold">1.0</div>
-          </div>
-        </div>
-        <b-button block variant="primary" class="mt-3">
-          <b-icon-download />
-          <span class="ml-2">Download client</span>
-        </b-button>
-      </b-card>
-      <b-card>
-        <div class="text-center">
-          <b-iconstack font-scale="3">
-            <b-icon stacked icon="circle-fill" variant="success"></b-icon>
-            <b-icon stacked icon="download" variant="light" scale="0.5"></b-icon>
-          </b-iconstack>
-        </div>
-        <h3 class="text-center mt-3">Latest patch</h3>
-        <div class="text-center">Full game patch</div>
-        <div class="mt-3">
-          <div class="d-flex justify-content-between">
-            <div>Size</div>
-            <div class="font-weight-bold">1 MB</div>
-          </div>
-          <div class="d-flex justify-content-between">
-            <div>Version</div>
-            <div class="font-weight-bold">0.1</div>
-          </div>
-        </div>
-        <b-button block variant="primary" class="mt-3">
-          <b-icon-download />
-          <span class="ml-2">Download patch</span>
-        </b-button>
-      </b-card>
-    </b-card-group>
+    <div class="d-flex align-items-center border-bottom vh-100">
+      <div class="w-100">
+        <h2 class="text-center">Download & Updates</h2>
+        <div class="text-center">Get the latest client and patches to start your adventure</div>
+        <b-card-group class="mt-4" deck>
+          <b-card>
+            <div class="text-center">
+              <b-iconstack font-scale="3">
+                <b-icon stacked icon="circle-fill" variant="success"></b-icon>
+                <b-icon stacked icon="download" variant="light" scale="0.5"></b-icon>
+              </b-iconstack>
+            </div>
+            <h3 class="text-center mt-3">Game client</h3>
+            <div class="text-center">Full game client</div>
+            <div class="mt-3">
+              <div class="d-flex justify-content-between">
+                <div>Size</div>
+                <div class="font-weight-bold">1 GB</div>
+              </div>
+              <div class="d-flex justify-content-between">
+                <div>Version</div>
+                <div class="font-weight-bold">1.0</div>
+              </div>
+            </div>
+            <b-button block variant="primary" class="mt-3">
+              <b-icon-download />
+              <span class="ml-2">Download client</span>
+            </b-button>
+          </b-card>
+          <b-card>
+            <div class="text-center">
+              <b-iconstack font-scale="3">
+                <b-icon stacked icon="circle-fill" variant="success"></b-icon>
+                <b-icon stacked icon="download" variant="light" scale="0.5"></b-icon>
+              </b-iconstack>
+            </div>
+            <h3 class="text-center mt-3">Latest patch</h3>
+            <div class="text-center">Full game patch</div>
+            <div class="mt-3">
+              <div class="d-flex justify-content-between">
+                <div>Size</div>
+                <div class="font-weight-bold">1 MB</div>
+              </div>
+              <div class="d-flex justify-content-between">
+                <div>Version</div>
+                <div class="font-weight-bold">0.1</div>
+              </div>
+            </div>
+            <b-button block variant="primary" class="mt-3">
+              <b-icon-download />
+              <span class="ml-2">Download patch</span>
+            </b-button>
+          </b-card>
+        </b-card-group>
+      </div>
+    </div>
   </div>
 </template>
 
